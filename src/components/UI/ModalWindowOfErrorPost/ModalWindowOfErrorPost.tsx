7ddb39e6d2d8cf25c8f39a99e109f276e00f94f1
DIEN94 <dm.kovalchyk94@gmail.com>
2023-07-16 18:17:22 +0300

    edit modal windows

--- src/components/UI/ModalWindowOfErrorPost/ModalWindowOfErrorPost.tsx
+++ src/components/UI/ModalWindowOfErrorPost/ModalWindowOfErrorPost.tsx
@@ -1,26 +1,23 @@
 import React, { FC } from "react";
 import { MyButton } from "../button/MyButton";
-import classes from "./ModalWindowOfErrorPost.module.css";
 import { IModal, MyModal } from "../MyModal/MyModal";
+import classes from "./ModalWindowOfErrorPost.module.css";
 
 interface IProps extends Omit<IModal,"children"> {}
 
-export const ModalWindowOfErrorPost = React.memo(({ ...props}: IProps) => {
+export const ModalWindowOfErrorPost = React.memo(({...props}: IProps) => {
   const exitModalWindow = () => {
     props.onClose();
   };
 
-  // if (!props.isOpen) return null;
   return (
-    <MyModal {...props}>
-      <div className={classes.modalWindowOfErrorPostContent}>
-        <p className={classes.text}>All fields must be filled!</p>
+    <MyModal {...props } className={classes.modalWindowOfErrorPostContent}>
+      <p className={classes.text}>All fields must be filled!</p>
         <div className={classes.buttonBox}>
           <MyButton onClick={exitModalWindow} className={classes.modalButton}>
             OK
           </MyButton>
         </div>
-      </div>
     </MyModal>
   );
 });
